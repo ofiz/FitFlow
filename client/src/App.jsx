2b5fc8bac75cd4c0454fdd50fa93c4ca1e61df1f
ofiz <ofircohen599@gmail.com>
2025-10-15 20:18:38 +0300

    reset password without tests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Register from './pages/Auth/Register'
-import Login from './pages/Auth/Login'   
-import Dashboard from './pages/Dashboard' 
+import Login from './pages/Auth/Login'
+import ForgotPassword from './pages/Auth/ForgotPassword'  
+import ResetPassword from './pages/Auth/ResetPassword'    
+import Dashboard from './pages/Dashboard'
 import './App.css'
 
 function App() {
@@ -13,12 +15,16 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />         
-          <Route path="/dashboard" element={<Dashboard />} /> 
+          <Route path="/login" element={<Login />} />
+          
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
     </Router>
   )
 }
 
-export default App;
+export default App
